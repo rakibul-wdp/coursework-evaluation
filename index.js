fbd0accd084c41790ce6ba922a8dda3254d1297d
rakibul-wdp <rakibul.wdp@gmail.com>
2022-12-15 15:16:30 +0600

    connect with database like mongodb mongoose

--- index.js
+++ index.js
@@ -5,10 +5,9 @@ const app = express()
 require('dotenv').config();
 const port = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`User and Post API listening on port ${port}`)
-})
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dcaauoo.mongodb.net/?retryWrites=true&w=majority`).then(data => {
+  console.log('Database connected');
+  app.listen(port);
+}).catch(err => {
+  console.log("Error occur in database", err);
+});
