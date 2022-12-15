ba867de375215d3896ea2087825d29def52bb979
rakibul-wdp <rakibul.wdp@gmail.com>
2022-12-15 15:19:43 +0600

    create simple route setup

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const app = express()
+const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dcaauoo.mongodb.net/?retryWrites=true&w=majority`).then(data => {
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dcaauoo.mongodb.net/?retryWrites=true&w=majority`)
+.then(data => {
   console.log('Database connected');
   app.listen(port);
-}).catch(err => {
+})
+.catch(err => {
   console.log("Error occur in database", err);
 });
