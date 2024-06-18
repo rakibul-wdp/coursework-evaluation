92fd4f22d093da1961584a7b5908847ae65b8304
rakibul-wdp <rakibul.wdp@gmail.com>
2024-06-19 01:39:00 +0600

    create pages

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,18 @@
+import ChatList from "../components/desktop/ChatList";
+import Message from "../components/desktop/Message";
+import MobileHomePage from "./MobileHomePage";
+
+const HomePage = () => {
+  return (
+    <main>
+      <div className="md:flex overflow-hidden hidden h-[100vh]">
+        <ChatList />
+        <Message />
+      </div>
+
+      <MobileHomePage />
+    </main>
+  );
+};
+
+export default HomePage;
