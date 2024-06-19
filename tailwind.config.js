bfad12c521ff6a8e3b564418d011f0d436ee05d0
rakibul-wdp <rakibul.wdp@gmail.com>
2024-06-20 03:39:00 +0600

    fix styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        darkChatBg: "url('./public/telegram-bg.png')",
+        lightChatBg: "url('./public/chat-light-bg.png')",
+      },
+    },
   },
-  plugins: [],
+  plugins: [import("flowbite/plugin")],
 };
