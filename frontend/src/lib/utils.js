3bc1aebb95c699b5263f2bece6a86c217bac4314
rakibul-wdp <rakibul.wdp@gmail.com>
2024-07-28 21:39:48 +0600

    fix backend live link

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -6,5 +6,5 @@ export function cn(...inputs) {
 }
 
 export function getStrapiURL() {
-  return process.env.NEXT_PUBLIC_STRAPI_URL ?? "http://localhost:1337";
+  return process.env.STRAPI_URL ?? "http://localhost:1337";
 }
