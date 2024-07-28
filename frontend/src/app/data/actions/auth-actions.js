fb80ad275213db1e4510e894cb58d7a6ee4e564b
rakibul-wdp <rakibul.wdp@gmail.com>
2024-07-28 16:33:00 +0600

    create auth action for sinin

--- frontend/src/app/data/actions/auth-actions.js
+++ frontend/src/app/data/actions/auth-actions.js
@@ -1,8 +1,11 @@
 "use server";
-import { z } from "zod";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
-import { registerUserService } from "../services/auth-service";
+import { z } from "zod";
+import {
+  loginUserService,
+  registerUserService,
+} from "../services/auth-service";
 
 const config = {
   maxAge: 60 * 60 * 24 * 7, // 1 week
@@ -25,8 +28,6 @@ const schemaRegister = z.object({
 });
 
 export async function registerUserAction(prevState, formData) {
-  console.log("Hello From Register User Action");
-
   const validatedFields = schemaRegister.safeParse({
     username: formData.get("username"),
     password: formData.get("password"),
@@ -64,4 +65,67 @@ export async function registerUserAction(prevState, formData) {
 
   cookies().set("jwt", responseData.jwt, config);
   redirect("/dashboard");
+}
+
+const schemaLogin = z.object({
+  identifier: z
+    .string()
+    .min(3, {
+      message: "Identifier must have at least 3 or more characters",
+    })
+    .max(20, {
+      message: "Please enter a valid username or email address",
+    }),
+  password: z
+    .string()
+    .min(6, {
+      message: "Password must have at least 6 or more characters",
+    })
+    .max(100, {
+      message: "Password must be between 6 and 100 characters",
+    }),
+});
+
+export async function loginUserAction(prevState, formData) {
+  const validatedFields = schemaLogin.safeParse({
+    identifier: formData.get("identifier"),
+    password: formData.get("password"),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      ...prevState,
+      zodErrors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Login.",
+    };
+  }
+
+  const responseData = await loginUserService(validatedFields.data);
+
+  if (!responseData) {
+    return {
+      ...prevState,
+      strapiErrors: responseData.error,
+      zodErrors: null,
+      message: "Ops! Something went wrong. Please try again.",
+    };
+  }
+
+  if (responseData.error) {
+    return {
+      ...prevState,
+      strapiErrors: responseData.error,
+      zodErrors: null,
+      message: "Failed to Login.",
+    };
+  }
+
+  cookies().set("jwt", responseData.jwt, config);
+
+  redirect("/dashboard");
+}
+
+export async function logoutAction() {
+  cookies().set("jwt", "", { ...config, maxAge: 0 });
+  redirect("/");
 }
