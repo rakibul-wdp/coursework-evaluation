214a13e4e2ff012f80af380b1bfbcabe07cd8f11
rakibul-wdp <rakibul.wdp@gmail.com>
2024-07-28 16:48:35 +0600

    create server

--- frontend/server.js
+++ frontend/server.js
@@ -1,35 +1,51 @@
-import { createServer } from "node:http";
+import { createServer } from "http";
+import next from "next";
 import { Server } from "socket.io";
-const cors = require("cors");
 
-const io = new Server(createServer, {
-  cors: {
-    origin: "http://localhost:3000", // Replace with your frontend URL
-    methods: ["GET", "POST"],
-    allowedHeaders: ["my-custom-header"],
-    credentials: true,
-  },
-});
+const dev = process.env.NODE_ENV !== "production";
+const hostname = "localhost";
+const port = 3000;
 
-io.on("connection", (socket) => {
-  console.log("A user connected:", socket.id);
-  socket.on("join_room", (roomId) => {
-    socket.join(roomId);
-    console.log(`user with id-${socket.id} joined room - ${roomId}`);
+const app = next({ dev, hostname, port });
+const handler = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const httpServer = createServer(handler);
+
+  const io = new Server(httpServer, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"],
+      allowedHeaders: ["my-custom-header"],
+      credentials: true,
+    },
   });
 
-  socket.on("send_msg", (data) => {
-    console.log(data, "DATA");
-    //This will send a message to a specific room ID
-    socket.to(data.roomId).emit("receive_msg", data);
+  io.on("connection", (socket) => {
+    console.log("A user connected:", socket.id);
+
+    socket.on("join_room", (roomId) => {
+      socket.join(roomId);
+      console.log(`User with id-${socket.id} joined room - ${roomId}`);
+    });
+
+    socket.on("send_msg", (data) => {
+      console.log(data, "DATA");
+      // This will send a message to a specific room ID
+      socket.to(data.roomId).emit("receive_msg", data);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("A user disconnected:", socket.id);
+    });
   });
 
-  socket.on("disconnect", () => {
-    console.log("A user disconnected:", socket.id);
-  });
-});
-
-const PORT = process.env.PORT || 3001;
-createServer.listen(PORT, () => {
-  console.log(`Socket.io server is running on port ${PORT}`);
+  httpServer
+    .once("error", (err) => {
+      console.error(err);
+      process.exit(1);
+    })
+    .listen(port, () => {
+      console.log(`> Ready on http://${hostname}:${port}`);
+    });
 });
