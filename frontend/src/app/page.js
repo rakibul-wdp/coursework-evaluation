9cd68c969613264b647dc3ee4580168ec95a4402
rakibul-wdp <rakibul.wdp@gmail.com>
2024-07-28 21:46:49 +0600

    remove home page dummy data

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,21 +1,6 @@
 import { SigninForm } from "@/components/forms/SigninForm";
 
-async function getStrapiData(url) {
-  const baseUrl = "http://localhost:1337";
-  try {
-    const response = await fetch(baseUrl + url);
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error(error);
-  }
-}
-
 export default async function Home() {
-  const strapiData = await getStrapiData("/api/home-page");
-
-  const { title, description } = strapiData.data.attributes;
-
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <SigninForm />
