e0b4b38ab9b824fdedffe3bcda7022dd9397815d
rakibul-wdp <rakibul.wdp@gmail.com>
2024-07-28 15:52:15 +0600

    for validate with zod

--- frontend/src/app/data/actions/auth-actions.js
+++ frontend/src/app/data/actions/auth-actions.js
@@ -1,15 +1,39 @@
 "use server";
 
-export async function registerUserAction(formData) {
+import { z } from "zod";
+
+const schemaRegister = z.object({
+  username: z.string().min(3).max(20, {
+    message: "Username must be between 3 and 20 characters",
+  }),
+  password: z.string().min(6).max(100, {
+    message: "Password must be between 6 and 100 characters",
+  }),
+  email: z.string().email({
+    message: "Please enter a valid email address",
+  }),
+});
+
+export async function registerUserAction(prevState, formData) {
   console.log("Hello From Register User Action");
 
-  const fields = {
+  const validatedFields = schemaRegister.safeParse({
     username: formData.get("username"),
     password: formData.get("password"),
     email: formData.get("email"),
-  };
+  });
 
-  console.log("#############");
-  console.log(fields);
-  console.log("#############");
+  if (!validatedFields.success) {
+    return {
+      ...prevState,
+      zodErrors: validatedFields.error.flatten().fieldErrors,
+      strapiErrors: null,
+      message: "Missing Fields. Failed to Register.",
+    };
+  }
+
+  return {
+    ...prevState,
+    data: "ok",
+  };
 }
--- frontend/src/components/forms/SignupForm.jsx
+++ frontend/src/components/forms/SignupForm.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import { useFormState } from "react-dom";
 
 import {
   Card,
@@ -11,14 +12,26 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
+import { registerUserAction } from "@/app/data/actions/auth-actions";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { registerUserAction } from "@/app/data/actions/auth-actions";
+import { ZodErrors } from "../custom/ZodErrors";
+
+const INITIAL_STATE = {
+  data: null,
+};
 
 export function SignupForm() {
+  const [formState, formAction] = useFormState(
+    registerUserAction,
+    INITIAL_STATE
+  );
+
+  console.log(formState);
+
   return (
     <div className="w-full max-w-md">
-      <form action={registerUserAction}>
+      <form action={formAction}>
         <Card>
           <CardHeader className="space-y-1">
             <CardTitle className="text-3xl font-bold">Sign Up</CardTitle>
@@ -35,6 +48,7 @@ export function SignupForm() {
                 type="text"
                 placeholder="username"
               />
+              <ZodErrors error={formState?.zodErrors?.username} />
             </div>
             <div className="space-y-2">
               <Label htmlFor="email">Email</Label>
@@ -44,6 +58,7 @@ export function SignupForm() {
                 type="email"
                 placeholder="name@example.com"
               />
+              <ZodErrors error={formState?.zodErrors?.email} />
             </div>
 
             <div className="space-y-2">
@@ -54,6 +69,7 @@ export function SignupForm() {
                 type="password"
                 placeholder="password"
               />
+              <ZodErrors error={formState?.zodErrors?.password} />
             </div>
           </CardContent>
           <CardFooter className="flex flex-col">
