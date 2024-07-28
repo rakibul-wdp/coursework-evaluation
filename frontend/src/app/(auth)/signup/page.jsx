5784b2e8182206e72ff25ed975e3d4e95af755bf
rakibul-wdp <rakibul.wdp@gmail.com>
2024-07-28 22:11:43 +0600

    decorate signup page

--- frontend/src/app/(auth)/signup/page.jsx
+++
@@ -1,5 +0,0 @@
-import { SignupForm } from "@/components/forms/SignupForm";
-
-export default function SingUpRoute() {
-  return <SignupForm />;
-}
---
+++ frontend/src/app/signup/page.jsx
@@ -0,0 +1,9 @@
+import { SignupForm } from "@/components/forms/SignupForm";
+
+export default function SingUpRoute() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <SignupForm />
+    </div>
+  );
+}
