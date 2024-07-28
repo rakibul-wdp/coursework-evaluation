e1ca724c49a9528fe9ff7f1c7085310e66578c0f
rakibul-wdp <rakibul.wdp@gmail.com>
2024-07-28 15:20:08 +0600

    create signup and signin forms

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,3 +1,6 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 async function getStrapiData(url) {
   const baseUrl = "http://localhost:1337";
   try {
@@ -18,6 +21,9 @@ export default async function Home() {
     <main className="container mx-auto py-6">
       <h1 className="text-5xl font-bold">{title}</h1>
       <p className="text-xl mt-4">{description}</p>
+      <Link href="/signin">
+        <Button>Sign In</Button>
+      </Link>
     </main>
   );
 }
