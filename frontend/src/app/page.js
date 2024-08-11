60bd5398db962b313b64ad89b6c40aab24d98594
rakibul-wdp <rakibul.wdp@gmail.com>
2024-08-12 01:39:00 +0600

    create new project

--- frontend/src/app/page.js
+++
@@ -1,9 +0,0 @@
-import { SigninForm } from "@/components/forms/SigninForm";
-
-export default async function Home() {
-  return (
-    <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <SigninForm />
-    </main>
-  );
-}
--- frontend/src/app/signup/page.jsx
+++
@@ -1,9 +0,0 @@
-import { SignupForm } from "@/components/forms/SignupForm";
-
-export default function SingUpRoute() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <SignupForm />
-    </div>
-  );
-}
